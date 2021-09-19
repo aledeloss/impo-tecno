7a7c32ad76a882737ef16ed1199c3e916827fad5
Aledeloss <Aledeloss@gmail.com>
2021-09-19 01:08:58 -0300

    controlador de productos completo v1

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,34 +1,75 @@
-const products = [
-  {
-    id: 1,
-    title: 'teles',
-  },
-];
+const Product = require('../models/Product');
 
-const getAllProducts = (req, res) => {
-  res.json({ message: 'hola getAllProducts ' });
+const getAllProducts = async (_, res) => {
+  try {
+    const data = await Product.find();
+    res.json(data);
+  } catch (e) {
+    console.error(e);
+  }
 };
 
 const createProduct = (req, res) => {
   console.log(req.body);
   const { category, trademark, name, price, stock, status } = req.body;
-  res.sendStatus(201).json();
-  res.json({ message: 'Producto creado' });
+  const newProduct = new Product({
+    category,
+    trademark,
+    name,
+    price,
+    stock,
+    status,
+  });
+  try {
+    newProduct.save((error) => {
+      if (error) {
+        res
+          .status(400)
+          .json({ message: 'La información ingresada no es válida' });
+      }
+      res.status(201).json({ message: 'Producto creado' });
+    });
+  } catch (err) {
+    console.error('HUBO UN ERROR', err);
+    res.status(404).send('La información ingresada no es válida');
+  }
 };
 
-const editProduct = (req, res) => {
-  res.json({ message: 'hola edit' });
+const singleProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log('id', id);
+    const data = await Product.findById(id);
+    res.json(data);
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(500);
+  }
 };
 
-// const find = async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const data = await Recipe.findById(id);
-//     res.json(data);
-//   } catch(e){
-//     console.error(e);
-//     res.sendStatus(500);
-//   };
-// };
+const editProduct = async (req, res) => {
+  const { id } = req.params;
+  const { category, trademark, name, price, stock, status } = req.body;
+  Product.findOne({ _id: id }, (err, prod) => {
+    if (err) {
+      console.error(err);
+      return res('Device update failed', null);
+    }
+    prod.category = category;
+    prod.trademark = trademark;
+    prod.name = name;
+    prod.price = price;
+    prod.stock = stock;
+    prod.status = status;
+    prod.ts_update = Date.now();
+    prod.save((err, prod) => {
+      if (err) {
+        console.error(err);
+      }
+      console.log('Se guardó el producto editado', prod);
+      res.json(prod);
+    });
+  });
+};
 
-module.exports = { getAllProducts, createProduct, editProduct };
+module.exports = { getAllProducts, createProduct, singleProduct, editProduct };
--- routes/products.js
+++ routes/products.js
@@ -4,12 +4,14 @@ const router = express.Router();
 const {
   getAllProducts,
   createProduct,
+  singleProduct,
   editProduct,
 } = require('../controllers/productsController');
 
 /* GET home page. */
 router.get('/all', getAllProducts);
 router.post('/create', createProduct);
-router.put('/:product/edit', editProduct);
+router.get('/:id/edit', singleProduct);
+router.put('/:id/edit', editProduct);
 
 module.exports = router;
