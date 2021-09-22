9d74710a7ba99470124e4701be628f35d69e0539
Aledeloss <Aledeloss@gmail.com>
2021-09-22 17:21:15 -0300

    pruebas de stockUpdate

---
+++ services/orderService.js
@@ -0,0 +1,66 @@
+const uuid = require('uuid'); //
+const Order = require('../models/Order');
+const Product = require('../models/Product');
+// const { createTicket } = require("../utils/pdfGen");
+
+const newOrder = async (req) => {
+  const { clientId, items } = req.body;
+
+  const validateOrder = items.map((item) => {});
+  const newOrder = new Order({
+    uuid: uuid(),
+    clientId,
+    items,
+  });
+};
+
+const approveOrderProducts = async (products) => {
+  const productsArray = products.map(({ id, price, quantity }) => {
+    const product = Product.find({
+      _id: id,
+      stock: { $gte: quantity },
+      enable: true,
+    });
+  });
+  console.log(product);
+  res.json({ result: product });
+};
+
+//   const [approvePurchaseResult] = await Promise.all(productsArray); // []
+//   if (approvePurchaseResult.length) console.log('orden ok'); // [[]]
+//   console.log('orden mal'); // return false;
+// };
+
+// const approveOrderProducts = async (products) => {
+//   const productsArray = products.map(({ id, price, quantity }) =>
+//     Product.find({
+//       _id: id,
+//       price: price,
+//       stock: { $gte: quantity },
+//       enable: true,
+//     })
+//   );
+// };
+
+const updateStock = async (products) => {
+  console.log(products);
+  try {
+    const result = products.map(({ id, quantity }) => {
+      Product.updateOne(
+        { _id: id },
+        {
+          $inc: { stock: -quantity },
+        }
+      );
+    });
+    const [updatedStock] = await Promise.all(result);
+    console.log(updatedStock);
+    return;
+  } catch (e) {
+    console.error(e);
+  }
+};
+module.exports = {
+  newOrder,
+  approveOrderProducts,
+};
