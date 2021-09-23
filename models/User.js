ff64f6cd053a3db6818a9531f1271380ed600d38
Aledeloss <Aledeloss@gmail.com>
2021-09-23 17:09:51 -0300

    user model

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    companyName: {
+      type: String,
+      required: true,
+    },
+    verificationCode: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      default: 'user',
+    },
+    enable: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = model('users', UserSchema);
