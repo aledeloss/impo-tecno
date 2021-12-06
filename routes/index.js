f5ad50403e0036035408a4628ae5912c1ed74eb1
Aledeloss <Aledeloss@gmail.com>
2021-12-05 22:42:52 -0300

    quité middlewares de autenticación para pruebas

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,9 @@ const productsRouter = require('./products');
 const { secureUser } = require('../middlewares/auth');
 
 router.use('/users', usersRouter);
-router.use('/products', secureUser, productsRouter);
-router.use('/orders', secureUser, ordersRouter);
+// router.use('/products', secureUser, productsRouter);
+// router.use('/orders', secureUser, ordersRouter);
+router.use('/products', productsRouter);
+router.use('/orders', ordersRouter);
 
 module.exports = router;
--- routes/orders.js
+++ routes/orders.js
@@ -16,12 +16,20 @@ const {
   cancelOrder,
 } = require('../controllers/ordersController');
 
-router.post('/create', secureUser, createOrder);
-router.get('/all', secureAdmin, getAllOrders);
-router.get('/my_orders', secureUser, getClientOrders);
-router.get('/:id', secureClientRoutes, getSingleOrder);
-router.get('/:id/edit', secureAdmin, getSingleOrder);
-router.put('/:id/edit', secureAdmin, validateOrderData, editOrder);
-router.put('/:id/cancel', secureAdmin, cancelOrder);
+// router.post('/create', secureUser, createOrder);
+// router.get('/all', secureAdmin, getAllOrders);
+// router.get('/my_orders', secureUser, getClientOrders);
+// router.get('/:id', secureClientRoutes, getSingleOrder);
+// router.get('/:id/edit', secureAdmin, getSingleOrder);
+// router.put('/:id/edit', secureAdmin, validateOrderData, editOrder);
+// router.put('/:id/cancel', secureAdmin, cancelOrder);
+
+router.post('/create', createOrder);
+router.get('/all', getAllOrders);
+router.get('/my_orders', getClientOrders);
+router.get('/:id', getSingleOrder);
+router.get('/:id/edit', getSingleOrder);
+router.put('/:id/edit', validateOrderData, editOrder);
+router.put('/:id/cancel', cancelOrder);
 
 module.exports = router;
