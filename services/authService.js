d8e1dc5e63d8213ed69e17fa3a9048848b6fe952
Aledeloss <Aledeloss@gmail.com>
2021-12-19 11:29:52 -0300

    cambio expiración de jwt a 90 días para pruebas

--- services/authService.js
+++ services/authService.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const signOptions = { expiresIn: '7 days', algorithm: 'RS256' };
+const signOptions = { expiresIn: '90 days', algorithm: 'RS256' };
 
 const createToken = (payload) => {
   return jwt.sign(payload, process.env.PRIVATE_KEY, signOptions);
@@ -10,3 +10,20 @@ const decodeToken = (token) => {
 };
 
 module.exports = { createToken, decodeToken };
+
+// const jwt = require('jsonwebtoken');
+// const fs = require('fs');
+
+// const privateKey = fs.readFileSync('./keys/private.pem');
+// const publicKey = fs.readFileSync('./keys/public.pem');
+
+// const signOptions = { expiresIn: '7 days', algorithm: 'RS256' };
+
+// const createToken = (payload) => {
+//   return jwt.sign(payload, privateKey, signOptions);
+// };
+// const decodeToken = (token) => {
+//   return jwt.verify(token, publicKey);
+// };
+
+// module.exports = { createToken, decodeToken };
